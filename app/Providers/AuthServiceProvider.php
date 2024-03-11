6296e615ec0a5baa77422c2f6af4308ca3ca1ce8
Judith <jcoeque299@g.educaand.es>
2024-03-11 09:36:01 +0100

    Creados roles

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
